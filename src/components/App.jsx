a85f8e09be73d0e5e467ee75032c1fbcd7bcb4a7
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-10-02 16:55:38 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,72 @@
 import { Component } from 'react';
-import ContactForm from './ContactForm/ContactForm';
-// import css from './App.module.css'
+import { nanoid } from 'nanoid';
+import s from './App.module.css';
+import Contacts from './Contacts/Contacts';
+import Phonebook from './Phonebook/Phonebook';
+import Section from './Section/Section';
+import Filter from './Filter/Filter';
 
-
-
-export class App extends Component {
+export default class App extends Component {
   state = {
     contacts: [],
     filter: '',
-    name: '',
-    number: '',
   };
 
-render () {
-  return (
-    <div>
-      <ContactForm />
-    </div>
-    
-  )
-}
-  
+  phonebookValue = (text, number) => {
+    const { contacts } = this.state;
 
-  
+    const contact = {
+      id: nanoid(),
+      name: text,
+      number,
+    };
+
+    if (contacts.map(e => e.name.toLowerCase()).includes(text.toLowerCase())) {
+      return alert(`${text} is already in contacts`);
+    }
+    if (text !== '' && number !== '') {
+      this.setState(prevState => {
+        return {
+          contacts: [...prevState.contacts, contact],
+        };
+      });
+    } else {
+      return alert('Fill in all the fields');
+    }
+  };
+
+  contactFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+  visibleContact = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(e =>
+      e.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  deleteList = e => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== e),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className={s.App}>
+        <Section title="Phonebook">
+          <Phonebook phonebookValue={this.phonebookValue} />
+        </Section>
+        <Section title="Contacts">
+          <Filter filter={this.contactFilter} />
+          <Contacts
+            contacts={this.visibleContact()}
+            deleteList={this.deleteList}
+          />
+        </Section>
+      </div>
+    );
+  }
 }
